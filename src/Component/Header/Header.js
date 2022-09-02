dd29eef84d28ac4f2f83ed1751ebd4586cf5c342
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2022-09-03 01:12:40 +0600

    nav and carousel done

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import  carousel1  from '../../images/carousel1.jpg';
+import carousel2 from '../../images/carousel2.jpg'
 import './Header.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Header = () => {
     return (
         <div className='navbar'>
             <nav>
                 <a href="./"><img src={logo} alt="" /></a>
-<input type="text" />
+                <form action="">
+                    <FontAwesomeIcon className='search' icon={faMagnifyingGlass} size="lg" />
+                    <input type="text" placeholder='Search anything'/>
+                </form>
+                <a href="./teach" className='nav-links teach'>Teach on Udemy</a>
+                <a href="./log-in" className='nav-links log-in'>Log in</a>
+                <a href="./sign" className='nav-links sign-up'>Sign up</a>
+                <a href="./cart" className='nav-links cart'><FontAwesomeIcon icon={ faShoppingCart} size="lg" /></a>
             </nav>
+            <div className='carousel'>
+                <Carousel>
+                    <Carousel.Item>
+                        <div className='billboard'>
+                            <h1>Learning that gets you</h1>
+                            <p>Skills for your present (and your future). Get started with us.</p>
+                        </div>
+                        <img
+                            className="d-block w-100"
+                            src={carousel1}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='billboard'>
+                            <h1>Unlock the power of people</h1>
+                            <p>Udemy is trusted by 10.5K+ companies around the world. </p>
+                        </div>
+                        <img
+                            className="d-block w-100"
+                            src={carousel2}
+                            alt="Second slide"
+                        />
+
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     );
 };
